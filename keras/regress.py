83a5235f308d9a92871e8448cd5e5d58f8afe248
Chris Pollard <cspollard@gmail.com>
2022-07-25 17:20:07 +0100

    needs work

--- keras/regress.py
+++ keras/regress.py
@@ -7,11 +7,11 @@ from sys import argv
 import shutil
 import json
 # import plotutils
-import utils
+# import utils
 import numpy
 
 
-print("torch version:", torch.__version__)
+print("keras version:", keras.__version__)
 
 if len(argv) < 2:
   print("please provide a json steering file")
@@ -56,18 +56,17 @@ runname = os.path.join(outdir, time_suffix)
 
 # always keep a copy of the steering file
 shutil.copyfile(argv[1], outdir + "/" + time_suffix + ".json")
-writer = SummaryWriter(runname)
 
 targlen = 1
 
-rng = np.random.default_rng()
+rng = numpy.random.default_rng()
 
 def generate_data(mus, sigs, norms, max_size):
   batches = norms.size
   ns = rng.poisson(norms)
 
-  mus = np.broadcast_to(mus, (max_size, 1, batches)).T
-  sigs = np.broadcast_to(sigs, (max_size, 1, batches)).T
+  mus = numpy.broadcast_to(mus, (max_size, 1, batches)).T
+  sigs = numpy.broadcast_to(sigs, (max_size, 1, batches)).T
 
   outs = mus + sigs * rng.standard_normal(size=(batches, 1, max_size))
 
@@ -83,11 +82,11 @@ def avg(l):
 
 ntests = 1000
 
-testsig_mu = avg(sig_mu_range) * np.ones(ntests)
-testsig_sigma = avg(sig_sigma_range) * np.ones(ntests)
+testsig_mu = avg(sig_mu_range) * numpy.ones(ntests)
+testsig_sigma = avg(sig_sigma_range) * numpy.ones(ntests)
 
-testbkg_mu = avg(bkg_mu_range) * np.ones(ntests)
-testbkg_sigma = avg(bkg_sigma_range) * np.ones(ntests)
+testbkg_mu = avg(bkg_mu_range) * numpy.ones(ntests)
+testbkg_sigma = avg(bkg_sigma_range) * numpy.ones(ntests)
 
 
 testtargs = \
@@ -111,7 +110,7 @@ testsigsigmas = \
   , size=ntests
   )
 
-testsiginputs = generate_data(testsigmus, testsigsigmas, testtargs[:,0], -1)
+testsiginputs = generate_data(testsigmus, testsigsigmas, testtargs[:,0], max_size)
 
 testbkgnorms = \
   rng.uniform \
@@ -134,7 +133,7 @@ testbkgsigmas = \
   , size=ntests
   )
 
-testbkginputs = generate_data(testbkgmus, testbkgsigmas, testbkgnorms, -1)
+testbkginputs = generate_data(testbkgmus, testbkgsigmas, testbkgnorms, max_size)
 for i in (range(n_bkgs-1)):
   testbkgnorms = \
     rng.uniform \
@@ -157,184 +156,208 @@ for i in (range(n_bkgs-1)):
     , size=ntests
     )
 
-  testbkginputs = testbkginputs.cat(generate_data(testbkgmus, testbkgsigmas, testbkgnorms, -1))
+  testbkginputs = testbkginputs.cat(generate_data(testbkgmus, testbkgsigmas, testbkgnorms, max_size))
 
 
 testinputs = testsiginputs.cat(testbkginputs)
 
-localnodes = [ 1 ] + localnodes
 
-globalnodes = \
-    [ localnodes[-1] + 1 ] \
-  + globalnodes \
-  + [ targlen + (targlen * (targlen+1) // 2) ]
+inputs = layers.Input(shape=(None, 1))
 
-act = torch.nn.LeakyReLU(0.01, inplace=True)
+outputs = inputs
+outputs = layers.Masking(mask_value=-MASKVAL)(outputs)
 
-localnet = \
-  [ torch.nn.Conv1d(localnodes[i], localnodes[i+1], 1) for i in range(len(localnodes) - 1) ]
+for nodes in localnodes:
+  outputs = layers.TimeDistributed(layers.Dense(nodes, activation='relu'))(outputs)
+
+outputs = Avg()(outputs)
+
+for nodes in globalnodes:
+  outputs = layers.Dense(nodes, activation='relu')(outputs)
+
+outputs = layers.Dense(2)(outputs)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.summary()
+
+model.compile \
+  ( loss = losses.BinaryCrossentropy(from_logits = True)
+  , optimizer = keras.optimizers.Adam(learning_rate=1e-4)
+  )
+
+
+# globalnodes = \
+#     [ localnodes[-1] + 1 ] \
+#   + globalnodes \
+#   + [ targlen + (targlen * (targlen+1) // 2) ]
+
+# act = torch.nn.LeakyReLU(0.01, inplace=True)
+
+# localnet = \
+#   [ torch.nn.Conv1d(localnodes[i], localnodes[i+1], 1) for i in range(len(localnodes) - 1) ]
   
-localnet = torch.nn.Sequential(*(utils.intersperse(act, localnet)))
+# localnet = torch.nn.Sequential(*(utils.intersperse(act, localnet)))
 
-globalnet = \
-  [ torch.nn.Linear(globalnodes[i], globalnodes[i+1]) for i in range(len(globalnodes) - 1) ]
+# globalnet = \
+#   [ torch.nn.Linear(globalnodes[i], globalnodes[i+1]) for i in range(len(globalnodes) - 1) ]
   
-globalnet = torch.nn.Sequential(*(utils.intersperse(act, globalnet)))
+# globalnet = torch.nn.Sequential(*(utils.intersperse(act, globalnet)))
 
-nets = [localnet , globalnet]
+# nets = [localnet , globalnet]
 
-# build the optimisers
-allparams = [ p for net in nets for p in net.parameters() ]
-optim = torch.optim.Adam(allparams, lr = lr)
-sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, factor=lrfactor, patience=patience)
+# # build the optimisers
+# allparams = [ p for net in nets for p in net.parameters() ]
+# optim = torch.optim.Adam(allparams, lr = lr)
+# sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, factor=lrfactor, patience=patience)
 
-for net in nets:
-  net.to(device)
+# for net in nets:
+#   net.to(device)
 
-os.mkdir(runname + ".plots")
+# os.mkdir(runname + ".plots")
 
-sumloss = 0
-sumdist = 0
-for epoch in range(number_epochs):
+# sumloss = 0
+# sumdist = 0
+# for epoch in range(number_epochs):
   
-  torch.save(localnet.state_dict(), runname + "/localnet.pth")
-  torch.save(globalnet.state_dict(), runname + "/globalnet.pth")
+#   torch.save(localnet.state_dict(), runname + "/localnet.pth")
+#   torch.save(globalnet.state_dict(), runname + "/globalnet.pth")
 
-  torch.save(localnet.state_dict(), runname + "/localnet.%03d.pth" % epoch)
-  torch.save(globalnet.state_dict(), runname + "/globalnet.%03d.pth" % epoch)
+#   torch.save(localnet.state_dict(), runname + "/localnet.%03d.pth" % epoch)
+#   torch.save(globalnet.state_dict(), runname + "/globalnet.%03d.pth" % epoch)
 
-  for net in nets:
-    net.training = False
+#   for net in nets:
+#     net.training = False
 
-  localnet.zero_grad()
-  globalnet.zero_grad()
-
-
-  if epoch > 0:
-
-    writer.add_scalar("learningrate", optim.param_groups[0]['lr'], global_step=epoch)
-    writer.add_scalar("avgloss", sumloss / epoch_size, global_step=epoch)
-    writer.add_scalar("avgdist", sumdist / epoch_size, global_step=epoch)
-    sched.step(sumloss / epoch_size)
+#   localnet.zero_grad()
+#   globalnet.zero_grad()
 
 
-  # mus , cov = utils.regress(localnet, globalnet, testinputs, 1)
-  # mus = mus.detach()
-  # cov = cov.detach()
+#   if epoch > 0:
 
-  # labels = [ "signalrate" ]
-  # binranges = [ sig_norm_range ]
+#     writer.add_scalar("learningrate", optim.param_groups[0]['lr'], global_step=epoch)
+#     writer.add_scalar("avgloss", sumloss / epoch_size, global_step=epoch)
+#     writer.add_scalar("avgdist", sumdist / epoch_size, global_step=epoch)
+#     sched.step(sumloss / epoch_size)
 
-  # plotutils.valid_plots \
-  #   ( mus.numpy()
-  #   , cov.numpy()
-  #   , testtargs
-  #   , labels
-  #   , binranges
-  #   , writer
-  #   , epoch
-  #   , None
-  #   )
 
-  print("starting epoch %03d" % epoch)
+#   # mus , cov = utils.regress(localnet, globalnet, testinputs, 1)
+#   # mus = mus.detach()
+#   # cov = cov.detach()
 
-  for net in nets:
-    net.training = True
+#   # labels = [ "signalrate" ]
+#   # binranges = [ sig_norm_range ]
 
-  sumloss = 0
-  sumdist = 0
-  for batch in range(epoch_size):
-    localnet.zero_grad()
-    globalnet.zero_grad()
+#   # plotutils.valid_plots \
+#   #   ( mus.numpy()
+#   #   , cov.numpy()
+#   #   , testtargs
+#   #   , labels
+#   #   , binranges
+#   #   , writer
+#   #   , epoch
+#   #   , None
+#   #   )
 
-    targs = \
-      rng.uniform \
-      ( low=sig_norm_range[0]
-      , high=sig_norm_range[1]
-      , size=(batch_size, 1)
-      )
+#   print("starting epoch %03d" % epoch)
 
-    sigmus = \
-      rng.uniform \
-      ( low=sig_mu_range[0]
-      , high=sig_mu_range[1]
-      , size=batch_size
-      )
+#   for net in nets:
+#     net.training = True
 
-    sigsigmas = \
-      rng.uniform \
-      ( low=sig_sigma_range[0]
-      , high=sig_sigma_range[1]
-      , size=batch_size
-      )
+#   sumloss = 0
+#   sumdist = 0
+#   for batch in range(epoch_size):
+#     localnet.zero_grad()
+#     globalnet.zero_grad()
 
-    siginputs = generate_data(sigmus, sigsigmas, targs[:,0], max_size)
+#     targs = \
+#       rng.uniform \
+#       ( low=sig_norm_range[0]
+#       , high=sig_norm_range[1]
+#       , size=(batch_size, 1)
+#       )
 
-    bkgnorms = \
-      rng.uniform \
-      ( low=bkg_norm_range[0]
-      , high=bkg_norm_range[1]
-      , size=batch_size
-      )
+#     sigmus = \
+#       rng.uniform \
+#       ( low=sig_mu_range[0]
+#       , high=sig_mu_range[1]
+#       , size=batch_size
+#       )
 
-    bkgmus = \
-      rng.uniform \
-      ( low=bkg_mu_range[0]
-      , high=bkg_mu_range[1]
-      , size=batch_size
-      )
+#     sigsigmas = \
+#       rng.uniform \
+#       ( low=sig_sigma_range[0]
+#       , high=sig_sigma_range[1]
+#       , size=batch_size
+#       )
 
-    bkgsigmas = \
-      rng.uniform \
-      ( low=bkg_sigma_range[0]
-      , high=bkg_sigma_range[1]
-      , size=batch_size
-      )
+#     siginputs = generate_data(sigmus, sigsigmas, targs[:,0], max_size)
 
-    bkginputs = generate_data(bkgmus, bkgsigmas, bkgnorms, max_size)
+#     bkgnorms = \
+#       rng.uniform \
+#       ( low=bkg_norm_range[0]
+#       , high=bkg_norm_range[1]
+#       , size=batch_size
+#       )
 
-    for i in (range(n_bkgs-1)):
-      bkgnorms = \
-        rng.uniform \
-        ( low=bkg_norm_range[0]
-        , high=bkg_norm_range[1]
-        , size=batch_size
-        )
+#     bkgmus = \
+#       rng.uniform \
+#       ( low=bkg_mu_range[0]
+#       , high=bkg_mu_range[1]
+#       , size=batch_size
+#       )
+
+#     bkgsigmas = \
+#       rng.uniform \
+#       ( low=bkg_sigma_range[0]
+#       , high=bkg_sigma_range[1]
+#       , size=batch_size
+#       )
+
+#     bkginputs = generate_data(bkgmus, bkgsigmas, bkgnorms, max_size)
+
+#     for i in (range(n_bkgs-1)):
+#       bkgnorms = \
+#         rng.uniform \
+#         ( low=bkg_norm_range[0]
+#         , high=bkg_norm_range[1]
+#         , size=batch_size
+#         )
     
-      bkgmus = \
-        rng.uniform \
-        ( low=bkg_mu_range[0]
-        , high=bkg_mu_range[1]
-        , size=batch_size
-        )
+#       bkgmus = \
+#         rng.uniform \
+#         ( low=bkg_mu_range[0]
+#         , high=bkg_mu_range[1]
+#         , size=batch_size
+#         )
   
-      bkgsigmas = \
-        rng.uniform \
-        ( low=bkg_sigma_range[0]
-        , high=bkg_sigma_range[1]
-        , size=batch_size
-        )
+#       bkgsigmas = \
+#         rng.uniform \
+#         ( low=bkg_sigma_range[0]
+#         , high=bkg_sigma_range[1]
+#         , size=batch_size
+#         )
 
-      bkginputs = bkginputs.cat(generate_data(bkgmus, bkgsigmas, bkgnorms, max_size))
+#       bkginputs = bkginputs.cat(generate_data(bkgmus, bkgsigmas, bkgnorms, max_size))
 
 
-    inputs = siginputs.cat(bkginputs)
+#     inputs = siginputs.cat(bkginputs)
 
-    mus , cov = utils.regress(localnet, globalnet, inputs, 1)
+#     mus , cov = utils.regress(localnet, globalnet, inputs, 1)
 
-    targs = torch.Tensor(targs).detach()
+#     targs = torch.Tensor(targs).detach()
 
-    l = utils.loss(targs, mus, cov)
+#     l = utils.loss(targs, mus, cov)
 
-    loss = l.mean()
+#     loss = l.mean()
 
-    loss.backward()
+#     loss.backward()
 
-    if grad_clip > 0:
-      torch.nn.utils.clip_grad_norm_(allparams, grad_clip)
+#     if grad_clip > 0:
+#       torch.nn.utils.clip_grad_norm_(allparams, grad_clip)
 
-    optim.step()
+#     optim.step()
 
-    sumloss += loss.detach().item()
-    sumdist += torch.sqrt((mus[:,0] - targs[:,0])**2).mean().detach().item()
+#     sumloss += loss.detach().item()
+#     sumdist += torch.sqrt((mus[:,0] - targs[:,0])**2).mean().detach().item()
 
